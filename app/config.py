bb2adf7f52a299fe01ceba2700019f4e4c02c181
jamesbaptist451-arch <jamesbaptist451@gmail.com>
2026-02-16 14:18:53 -0400

    Delete app/config.py

--- app/config.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-DATABASE_URL = "postgresql://postgres:5378@localhost:5432/marketintel"
-BINANCE_WS = "wss://stream.binance.com:9443/ws/btcusdt@aggTrade"
